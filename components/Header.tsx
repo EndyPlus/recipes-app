27c9b28f265b9a11b0c8f1a0cbf8e4599ae197b8
Artem <aapliusnin@gmail.com>
2023-05-01 22:31:53 +0300

    updated main-search layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ export default function Header() {
     return (
         <header>
             {/*Container*/}
-            <div className="px-24 flex justify-between p-5 text-indigo-500 bg-indigo-700">
+            <div className="px-24 flex justify-between p-5 text-indigo-500 bg-indigo-700 shadow-xl shadow-indigo-300">
             {/*Logo*/}
-                <a className="font-bold text-5xl text-white p-3 border border-indigo-200 rounded-full bg-indigo-600 cursor-pointer hover:text-indigo-600 hover:bg-white hover:border-indigo-900">Recipes</a>
+                <a className="flex font-bold text-5xl text-white p-3 border border-indigo-200 rounded-full bg-indigo-600 cursor-pointer hover:text-indigo-600 hover:bg-white hover:border-indigo-900">Recipes</a>
             {/*Navigation*/}
                 <nav className="flex items-center text-xl font-semibold">
                 <ul className="inline-flex items-center text-indigo-200 hover:text-white cursor-pointer">
--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -4,33 +4,46 @@ import TestImg from "../assets/recipeimg.jpg";
 export default function MainPage() {
     return (
         <div>
+
+
             {/*Search block*/}
             <div
-                className="flex flex-col bg-indigo-200 border border-indigo-500 rounded-[100px] m-10 justify-center items-center">
-                <form className="flex m-5 border-b border-indigo-500 flex-col ">
-                    <label htmlFor="searchR" className="flex font-semibold uppercase px-[50%]">Search:</label>
-                    <input type="search" id="searchR" name="searchRecipes" className="flex mx-96"/>
+                className="flex flex-col m-10 items-center">
+                <form className="group flex pt-5 px-24 m-5 flex-col text-4xl">
+                    <label htmlFor="searchR" className="flex font-semibold uppercase self-center pb-5 group-hover:text-indigo-500">Search:</label>
+                    <input type="search" id="searchR" name="searchRecipes" className="flex self-center placeholder:text-indigo-500 border-2 border-indigo-900 rounded-full w-full py-3 px-3 mx-36 shadow-lg group-hover:text-indigo-600 focus:outline-none group-hover:border-indigo-200 group-hover:ring-indigo-300 group-hover:ring-2 sm:text-sm"/>
                 </form>
-                <div className="grid grid-cols-5 content-center py-5 border border-indigo-500">
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
-                    <button className="maintag">tag</button>
+                <div className="flex-col mt-3">
+                    <h2 className="text-xl font-bold flex justify-center">U can fill this input with such tags as:</h2>
+                    <div className="grid grid-cols-6 content-center px-8 pb-5 pt-3 my-5 text-lg font-semibold border-y-2 rounded-3xl border-y-indigo-500">
+                        <button className="py-2 px-3 mx-10 my-2 w-24 justify-self-center border border-indigo-300 rounded-xl bg-indigo-100 text-indigo-500 hover:bg-indigo-500 hover:text-white hover:underline">Dinners</button>
+                        <button className="maintag">Meals</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                        <button className="maintag">tag</button>
+                    </div>
                 </div>
-                <button className="font-semibold text-indigo-500 border border-indigo-700 my-5 py-2 px-3">I`m feeling
-                    lucky
-                </button>
+                <button className="font-semibold text-3xl border-2 rounded-full bg-white text-indigo-700 hover:text-white hover:bg-indigo-700  border-indigo-700 my-5 py-2 px-3">Search random recipe</button>
             </div>
 
+
+
             {/*Explore Section*/}
-            <div>
-                <h1 className="font-bold text-xl uppercase text-indigo-700 border-b border-indigo-500 pb-5">Explore
+            <div className="border-t-2 pt-10 border-indigo-500">
+                <h1 className="flex justify-center font-bold text-3xl uppercase text-indigo-700 pb-5">Explore
                     recipes</h1>
                 {/*Cards list*/}
                 <div className="grid grid-cols-3 m-4 justify-evenly gap-5">
@@ -42,8 +55,6 @@ export default function MainPage() {
                             <p className="text-indigo-400">Description</p>
                         </div>
                     </div>
-
-
                     <div className="border bg-white rounded-lg overflow-hidden">
                         <Image src={TestImg} alt="Recipe" className="w-full h-48 object-cover"/>
                         <div className="p-4">
